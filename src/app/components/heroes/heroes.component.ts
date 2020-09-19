e66603262158b2c010f355f3960e7357ee0231b3
Cristian López <cristian_210199@hotmail.com>
2020-09-19 15:42:40 -0500

    app completa

---
+++ src/app/components/heroes/heroes.component.ts
@@ -0,0 +1,31 @@
+import { Component, OnInit } from '@angular/core';
+import { HeroesService } from 'src/app/services/heroes.service';
+import { Router } from '@angular/router';
+
+@Component({
+  selector: 'app-heroes',
+  templateUrl: './heroes.component.html'
+})
+export class HeroesComponent implements OnInit {
+  heroes: Heroe[] = [];
+  constructor(private _heroeService: HeroesService,
+    private router: Router) { }
+
+  ngOnInit(): void {
+    this.heroes = this._heroeService.getHeroes();
+  }
+
+  verHeroe(i: number){
+    this.router.navigate(['/heroe', i]);
+  }
+
+}
+
+interface Heroe{
+  nombre: string;
+  img: string;
+  aparicion: string;
+  casa: string;
+  bio: string;
+}
+
